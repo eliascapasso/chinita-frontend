2ded9beec49b3e2e0f76b7d0e5f4bfa0c83fdf3d
eliascapasso <elias_capasso@live.com>
2021-10-28 11:21:26 -0300

    boton wsp

--- src/app/core/content/content.component.ts
+++ src/app/core/content/content.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Subscription } from "rxjs";
+import { AuthService } from "../../account/shared/auth.service";
+import { User } from "../../models/user.model";
+import { ContactService } from "../shared/contact.service";
 
-import { OffcanvasService } from '../shared/offcanvas.service';
+import { OffcanvasService } from "../shared/offcanvas.service";
 
 @Component({
-  selector: 'app-content',
-  templateUrl: './content.component.html',
-  styleUrls: ['./content.component.scss']
+  selector: "app-content",
+  templateUrl: "./content.component.html",
+  styleUrls: ["./content.component.scss"],
 })
 export class ContentComponent {
-  constructor(private offcanvasService: OffcanvasService) {}
+  private userSubscription: Subscription;
+  public user: User;
+  public wsp: string = "";
+
+  constructor(
+    public contactService: ContactService,
+    private offcanvasService: OffcanvasService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.userSubscription = this.authService.user.subscribe((user) => {
+      this.user = user;
+    });
+
+    this.contactService.getContact().subscribe((contact) => {
+      this.wsp = contact.wsp;
+    });
+  }
 
   onMenuClose(e: Event) {
     this.offcanvasService.closeOffcanvasNavigation();
